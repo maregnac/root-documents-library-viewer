f579160cd981df7f319515e0aaf42b78a0a43c88
Dmitry Mareniak <marenyak@southernoceanco.com>
2025-01-12 18:25:26 +0200

    First

---
+++ src/webparts/rootDocumentLibraryViewer/components/IRootDocumentLibraryViewerProps.ts
@@ -0,0 +1,7 @@
+export interface IRootDocumentLibraryViewerProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/rootDocumentLibraryViewer/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IRootDocumentLibraryViewerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'RootDocumentLibraryViewerWebPartStrings' {
+  const strings: IRootDocumentLibraryViewerWebPartStrings;
+  export = strings;
+}
